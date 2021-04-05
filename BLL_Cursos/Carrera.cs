829ba7f22defeeb66ee2ec1758c85957592c9d07
Matias L. Bassi <matiasb_h_@hotmail.com>
2021-04-05 20:50:07 -0300

    Commit inicial con division en 3 capas

---
+++ BLL_Cursos/Carrera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    class Carrera
+    {
+        private string _nombre;
+        private string _coordinador;
+        private int _duracionEnCuatris;
+        private HashSet<Materia> _materias = new HashSet<Materia>();
+
+
+        public Carrera( string nombre )
+        {
+            this._nombre = nombre;
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; } 
+        }
+
+
+
+        public int CantidadDeCuatrimestres
+        {
+            get { return _duracionEnCuatris; }
+            set { _duracionEnCuatris = value; }
+        }
+
+
+        public string Coordinador
+        {
+            get { return _coordinador; }
+            set { _coordinador = value; }
+        }
+
+
+        public bool AgregarMateria( Materia nuevaMateria )
+        {
+            return this._materias.Add( nuevaMateria );
+        }
+    }
+}
---
+++ BLL_Cursos/Materia.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    class Materia
+    {
+        private string _nombre;
+        private Carrera _carrera;
+        private int _sumaCuatris;
+
+        
+
+        public Materia( string nombre )
+        {
+            this._nombre = nombre;
+        }
+
+
+        public Carrera Carrera
+        {
+            get { return _carrera; }
+            set {
+                if (value != null && 
+                    value.AgregarMateria( this ) )
+                {
+                    this._carrera = value;
+                }
+            }
+        }
+
+
+        public bool SetMomentoDeLaCarrera( Carrera carrera, int anio, int cuatrimestre)
+        {
+            this.Carrera = carrera;
+            return this.SetMomentoDeLaCarrera(anio, cuatrimestre);
+        }
+
+
+        public bool SetMomentoDeLaCarrera( int anio, int cuatrimestre )
+        {
+            int conversion;
+
+            if (anio < 1 || cuatrimestre < 1 || cuatrimestre > 2)
+                return false;
+
+            anio--;
+            // Pasaje de 1 año a 2 cuatrimestres más los cuatrimestres que se indiquen
+            conversion = anio * 2 + cuatrimestre;
+
+            if (this.Carrera == null ||
+                conversion > this.Carrera.CantidadDeCuatrimestres)
+                return false;
+
+            this._sumaCuatris = conversion;
+            return true;
+        }
+
+
+        public int Anio
+        {
+            get { return (this._sumaCuatris + 1) / 2; }
+        }
+
+        public int Cuatrimestre
+        {
+            get { return (this._sumaCuatris + 1) % 2 + 1 ; }
+        }
+
+
+        public bool Persistir()
+        {
+            return true;
+        }
+    }
+}
